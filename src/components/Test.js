07537e61158930a008b0f5f617568f194da9b00e
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-08 19:09:55 +0000

    Mess around with react and websockets

--- src/components/Test.js
+++ src/components/Test.js
@@ -2,17 +2,7 @@ import React, { Component } from 'react';
 
 // const host = location.host;
 // console.log(host);
-const ws = new WebSocket(`ws://localhost:3000`);
 
-ws.onopen = function(){
-
-  function sendMessage(num){
-    var increment = parseInt(num)
-    increment += 1
-    ws.send(increment);
-  };
-  // setInterval(sendMessage, 2000);
-};
 
 class Test extends Component {
   constructor(props) {
@@ -20,23 +10,28 @@ class Test extends Component {
     this.state = {
       number: 0
     }
+    this.ws = new WebSocket(`ws://localhost:3000`);
+    this.sendMessage = this.sendMessage.bind(this)
     this.activateLasers = this.activateLasers.bind(this);
     this.handleData = this.handleData.bind(this);
   }
 
+  sendMessage() {
+    this.ws.send(this.state.number + 1);
+  }
+
   activateLasers() {
-    this.setState({number: this.state.number + 1});
+    this.setState({ number: this.state.number + 1 });
   }
 
   handleData(data) {
     console.log(data);
   }
 
-
   render() {
     return (
       <div>
-        <button onClick={sendMessage(this.state.number)}>
+        <button onClick={ this.sendMessage }>
           Activate Lasers
         </button>
         <h1>HELLO {this.state.number}</h1>
